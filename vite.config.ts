8e7042c1efd64b5d929a352d4d422aa7ba76ab06
varaeff <varaev.sergey@gmail.com>
2024-12-06 22:17:39 +0400

    build

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: '/BotanInvestments-test-task/',
-  base: './',
+  base: '/BotanInvestments-test-task/',
   plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
@@ -17,5 +16,8 @@ export default defineConfig({
   },
   define: {
     'process.env': process.env,
+  },
+  build: {
+    outDir: 'dist',
   },
 })
