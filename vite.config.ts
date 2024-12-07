4df6afb935f2a8ece607c354117803daa8fb7c11
varaeff <varaev.sergey@gmail.com>
2024-12-07 14:12:25 +0400

    404.html addded for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,23 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
-// https://vite.dev/config/
 export default defineConfig({
   base: '/BotanInvestments-test-task/',
-  plugins: [vue(), vueJsx(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    vueDevTools(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/404.html',
+          dest: '.',
+        },
+      ],
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
